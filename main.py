b6a664bbe4f3cb97b8c4fa7503f8b9a18f4dc49b
Ivan Pavlenko <ivan_pavlenko@knu.ua>
2024-02-11 17:51:52 +0200

    some little changes

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from utils import *
 
-
 print(factorial(5))
 
-<<<<<<< HEAD
 print(nsd(12, 11))
-=======
 
 print(is_prime(6))
->>>>>>> 0935754cc0c1464c9d3d5207ac11cf7b7fad72d5
+
+print(is_power_of_five(626))
--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
-<<<<<<< HEAD
 
 
 def nsd(a: int, b: int):
@@ -17,15 +16,13 @@ def nsd(a: int, b: int):
         return a
 
     return nsd(b, a % b)
-=======
-<<<<<<< HEAD
 
 
 def is_power_of_five(number):
     import math
     return (number > 0) and (math.log(number, 5) % 1 == 0)
-=======
-    
+
+
 def is_prime(number):
     if number <= 1:
         return False
@@ -39,5 +36,4 @@ def is_prime(number):
             return False
         i += 6
     return True
->>>>>>> 80459f2d960441a6f8445dbb917fbcb82ae5bd66
->>>>>>> 0935754cc0c1464c9d3d5207ac11cf7b7fad72d5
+
