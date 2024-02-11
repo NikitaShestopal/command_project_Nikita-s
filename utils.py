31707cc50f3da53189ea9b084ef48b87758a2752
Paschenko Maksym <maksympash@knu.ua>
2024-02-11 17:45:40 +0200

    Added power_5

--- utils.py
+++ utils.py
@@ -3,3 +3,8 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+
+
+def is_power_of_five(number):
+    import math
+    return (number > 0) and (math.log(number, 5) % 1 == 0)
