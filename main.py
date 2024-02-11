085db960cdc258444d445e0bc7d3ded0d7638349
Paschenko Maksym <maksympash@knu.ua>
2024-02-11 17:58:08 +0200

    Added decimal_to_binary

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ print(nsd(12, 11))
 print(is_prime(6))
 
 print(is_power_of_five(626))
+
+print(decimal_to_binary(66))
--- utils.py
+++ utils.py
@@ -37,3 +37,12 @@ def is_prime(number):
         i += 6
     return True
 
+def decimal_to_binary(decimal_number):
+    if decimal_number == 0:
+        return '0'
+    binary_number = ''
+    while decimal_number > 0:
+        remainder = decimal_number % 2
+        binary_number = str(remainder) + binary_number
+        decimal_number //= 2
+    return binary_number
