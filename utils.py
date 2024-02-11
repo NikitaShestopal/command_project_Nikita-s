6419fa0deaab6c4ed62dd53fcd711c010379faf3
John Doe <snikita2206@gmail.com>
2024-02-11 18:01:02 +0200

    bublle_sorting

--- utils.py
+++ utils.py
@@ -37,3 +37,9 @@ def is_prime(number):
         i += 6
     return True
 
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
